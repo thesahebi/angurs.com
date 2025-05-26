f3d6af1fb07b986b5c4707ba663949754501ee68
thesahebi <sahebiramazan@gmail.com>
2025-05-26 10:21:44 -0500

    changed header text

--- src/components/pages/hero/HeroV2.tsx
+++ src/components/pages/hero/HeroV2.tsx
@@ -89,10 +89,10 @@ function HeroV2() {
         <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div className="max-w-3xl mx-auto text-center">
             <p ref={taglineRef} className="inline-flex px-4 py-2 text-base text-gray-900 border border-gray-200 rounded-full font-pj">
-              A Digital Creative Agency
+              Cloud and Security Solutions
             </p>
             <h1 ref={titleRef} className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj">
-              Angurs Cloud & Security Solutions
+              Angurs
             </h1>
             <p ref={subtitleRef} className="max-w-md mx-auto mt-6 text-base leading-7 text-gray-600 font-inter">
               Helping your business stay connected, secure, and supported.
