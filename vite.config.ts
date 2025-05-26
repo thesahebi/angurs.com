dad25bcb66c1024823a1cce8129580c6c409077e
thesahebi <sahebiramazan@gmail.com>
2025-05-26 10:32:18 -0500

    increased ts chunk suze

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-
-
 import path from "path";
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from "vite";
  
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'gsap'],
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
