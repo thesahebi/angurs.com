4505d93f4cd4facfed4a18be79464758b999a80d
dev01 <thesahebi>
2025-09-20 01:29:44 +0000

    changed font and homepage

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -4,7 +4,6 @@ import FeatureV1 from "./features/FeatureV1";
 import { ClientV1 } from "./ClientV1";
 import { CustomerRating } from "./CustomerRating";
 
-import { VelocityScroll } from "../ui/scroll-based-velocity";
 import WorkTogether from "./WorkTogether";
 import HeroV2 from "./hero/HeroV2";
 
@@ -13,14 +12,6 @@ const LandingPage = () => {
     <main>
       {/* <HeroV1 /> */}
       <HeroV2 />
-  
-
-      <VelocityScroll
-        default_velocity={2} 
-        text="Beautiful Web Design • Smart Software Development • Seamless Server Migration • Scalable Microservices • Cloud Solutions That Grow • "
-        className="text-white text-lg md:text-2xl lg:text-3xl xl:text-4xl font-medium tracking-wide"
-      />
-
       <FeatureV1 />
 
       <WorkTogether />
